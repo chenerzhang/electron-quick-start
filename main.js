bc02ea69be1c4d8dd120f3a01be2983266c60d70
chenerzhang <chenerzhang@bytedance.com>
2019-03-07 14:32:06 +0800

    test
    Change-Id: I59ed2883385b03b3ba20bb8091f0ac83d9ebdf14

--- main.js
+++ main.js
@@ -1,20 +1,68 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, protocol} = require('electron');
+const {app, BrowserWindow, protocol, session} = require('electron');
 const path = require('path');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-protocol.registerStandardSchemes(['miniapp']);
+const CUSTOM_SCHEME = 'miniapp';
+const LOCAL_HTML_PATH = '/Users/cez/code/github/electron-quick-start/html';
+protocol.registerStandardSchemes([CUSTOM_SCHEME]);
 app.on('ready', () => {
-  protocol.registerFileProtocol('miniapp', (request, callback) => {
-    const url = request.url;
-    console.log(url);
-    callback(path.normalize('/Users/cez/code/test/1.html'));
-  }, (error) => {
-    console.error('registerFileProtocol 1 error', error);
+  const partition1 = 'persist:webview1';
+  const partition2 = 'persist:webview2';
+  const ses1 = session.fromPartition(partition1);
+  const ses2 = session.fromPartition(partition2);
+  
+  const ses1Url = `${CUSTOM_SCHEME}://webview1.com`;
+  ses1.cookies.set({
+    url: ses1Url,
+    name: 'webview1-cookie',
+    value: 'wenview1'
+  }, (err) => {
+    if (err) console.error('set webview1 cookie error', err);
   });
+  ses1.protocol.registerFileProtocol(CUSTOM_SCHEME, (request, callback) => {
+    const url = request.url.replace(ses1Url, '');
+    console.log(url);
+    callback(path.normalize(`${LOCAL_HTML_PATH}${url}`));
+  }, (error) => {
+    if (error) {
+      console.error('registerFileProtocol 1 error', error);
+    }
+  });
+
+  const ses2Url = `${CUSTOM_SCHEME}://webview2.com`;
+  ses2.cookies.set({
+    url: ses2Url,
+    name: 'webview2-cookie',
+    value: 'wenview2'
+  }, (err) => {
+    if (err) console.error('set webview2 cookie error', err);
+  });
+  ses2.protocol.registerFileProtocol(CUSTOM_SCHEME, (request, callback) => {
+    const url = request.url.replace(ses2Url, '');
+    console.log(url);
+    callback(path.normalize(`${LOCAL_HTML_PATH}${url}`));
+  }, (error) => {
+    if (error) {
+      console.error('registerFileProtocol 2 error', error);
+    }
+  });
+
+  // 设置主端 cookie
+  const mainCookie = {
+    url: 'https://zjurl.cn',
+    name: 'main-cookie',
+    value: 'main'
+  };
+  session.defaultSession.cookies.set(mainCookie, (err) => {
+    if (err) console.error('set main cookie error', err);
+  });
+  session.defaultSession.cookies.get({}, (error, cookies) => {
+    console.log(error, cookies)
+  })
 });
 
 function createWindow () {
--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+setTimeout(() => {
+  const $webview1 = document.querySelector('#webview1');
+  const $webview2 = document.querySelector('#webview2');
+  const $webview3 = document.querySelector('#webview3');
+
+  $webview1.openDevTools();
+  $webview2.openDevTools();
+  $webview3.openDevTools();
+}, 2000);
